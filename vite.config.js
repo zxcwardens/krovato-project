d7179f49c1a02cdd6ec8bc4485e96f68140453c9
Artem <126970937+indozer@users.noreply.github.com>
2024-03-22 23:47:25 +0200

    deploy settings

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,27 @@
-import { defineConfig } from 'vite'
-import glob from 'glob'
-import injectHTML from 'vite-plugin-html-inject'
-import FullReload from 'vite-plugin-full-reload'
+import { defineConfig } from "vite";
+import glob from "glob";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
 
 export default defineConfig({
-define: {
-global: {},
-},
-root: 'src',
-build: {
-rollupOptions: {
-input: glob.sync('./src/*.html'),
-},
-outDir: '../dist',
-},
-plugins: [injectHTML(), FullReload(['./src/index.html'])],
-})
+  define: {
+    global: {},
+  },
+  root: "src",
+  build: {
+    rollupOptions: {
+      input: glob.sync("./src/*.html"),
+    },
+    outDir: "../dist",
+  },
+  plugins: [
+    injectHTML({
+      injectData: {
+        title: 'Krovato', 
+      },
+      indexFileName: './src/index.html'
+    }), 
+    FullReload(["./src/**.html"])
+  ],
+});
+    
